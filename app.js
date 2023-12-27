2cacf8fdc67c402512ba36f8b71d88bfd39276a6
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-27 19:19:10 +0100

    ajout middleware principal et tableau objet

--- app.js
+++ app.js
@@ -6,26 +6,39 @@ const express = require('express');
 // Creation const express
 const app = express();
 
-// ***** Parametrage des requetes (middleware) avec next qui renvoie au prochain middleware *******
+// ***** Parametrage middleware avec next qui renvoie au prochain middleware *******
+
+// middleware general car pas de route URL
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
   });
 
+// avec URL demander par le front
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+    next();
+  });
 // Export const pour etre utiliser pas les autres fichiers du projet
 module.exports = app;
 
