09dc87e00c10226ae1ec604ded0418c6000e18d3
ThomasVdbk <thomas.vdbk@gmail.com>
2023-12-29 17:45:11 +0100

    déportation des middlewares routes app>stuff

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const express = require('express');
 const app = express();
 // Import mongoose
 const mongoose = require('mongoose');
-// Import schema donnée
-const Thing = require('./models/Thing');
-
+// Import du routeur
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect('mongodb+srv://JohnMcClane:dieHard@cluster0.mwtizxt.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -19,6 +18,7 @@ mongoose.connect('mongodb+srv://JohnMcClane:dieHard@cluster0.mwtizxt.mongodb.net
   
   // ***** Parametrage middleware avec next qui renvoie au prochain middleware *******
   
+
   // middleware general car pas de route URL
   app.use((req, res, next) => {
       //Ces headers permettent :
@@ -34,42 +34,9 @@ mongoose.connect('mongodb+srv://JohnMcClane:dieHard@cluster0.mwtizxt.mongodb.net
 // middleware intercepte toutes les requetes contenant du json pour le mettre a dispo de req.body via app.post
 app.use(express.json());
 
-
-// middleware qui envoi vers la bdd en utilisant le schema Thing
-app.post('/api/stuff', (req, res, next) => {
-  // suppression du champ id genere par mongodb
-  delete req.body._id;
-  // creation d'un nouveau Thing en copiant le corp de la request avec spread
-  const thing = new Thing({
-    ...req.body
-  });
-  // enregistement du thing cree dans la bdd avec Promise (renvoyé par save())
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-    });
-
-// middleware update Thing
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-   });
-
-// middleware delete Thing
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-// avec URL demander par le front pour lecture bdd
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-  });
+// Pour cette route là import via stuffRoutes
+app.use('/api/stuff', stuffRoutes);
+  
 // Export const pour etre utiliser pas les autres fichiers du projet
 module.exports = app;
 
